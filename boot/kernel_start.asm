232fc368ced9e5cc66f34a44a6be52e2d9861a47
wendysegura <118@holbertonschool.com>
2018-02-19 11:08:45 -0800

    Update kernel_start.asm

--- boot/kernel_start.asm
+++ boot/kernel_start.asm
@@ -1,4 +1,4 @@
 [bits 32]
 [extern main] ; Define calling point, must be same name as kernel.c 'main' function
-call main ; Calls the C function.
+call main ; Calls the C function.The linker will know where it is placed in memory
 jmp $
